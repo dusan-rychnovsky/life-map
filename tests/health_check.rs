ddb2ebaff6f9ea40af82b9cfa67002dccd79ef15
Dušan Rychnovský <dusan.rychnovsky@gmail.com>
2025-10-02 08:40:28 +0200

    Setup integration tests.

---
+++ tests/health_check.rs
@@ -0,0 +1,18 @@
+#[tokio::test]
+async fn health_check_returns_200() {
+    spawn_app();
+    let client = reqwest::Client::new();
+    let response = client
+        .get("http://127.0.0.1:8080/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = life_map::run().expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+}
