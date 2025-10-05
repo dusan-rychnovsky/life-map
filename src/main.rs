7f14bf9e2413973182063d37ef4b4084d3bb8381
Dušan Rychnovský <dusan.rychnovsky@gmail.com>
2025-10-05 08:45:18 +0200

    Config.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use life_map::configuration::get_configuration;
 use life_map::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run(TcpListener::bind("127.0.0.1:8080")?)?.await
+    let config = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", config.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
